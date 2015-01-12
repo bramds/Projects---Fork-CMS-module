24c6e2c33f0aa479ab229935ad148f5e2337e540
Bram De Smyter <bram@bubblefish.be>
2015-01-12 13:24:16 +0100

    Update detail.js

--- src/Frontend/Modules/Projects/Js/detail.js
+++ src/Frontend/Modules/Projects/Js/detail.js
@@ -1,12 +1,40 @@
-$(document).ready(function() {
-	$(".colorbox").colorbox({
-        maxWidth: '95%',
-        maxHeight: '95%',
-        current: jsData.projects.current,
-        previous: jsData.projects.previous,
-        next: jsData.projects.next,
-        close: jsData.projects.close,
-        xhrError: jsData.projects.xhrError,
-        imgError: jsData.projects.imgError
-    });
-});
+/*
+ * This file is part of Fork CMS.
+ *
+ * For the full copyright and license information, please view the license
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Interaction for the projects module
+ *
+ * @author Bart De Clercq <info@lexxweb.be>
+ * @author Bram De Smyter <bram@bubblefish.be>
+ */
+jsFrontend.projectsDetail =
+{
+	// constructor
+	init: function()
+	{
+        //--Initialize colorbox to the gallery
+        $('.colorbox').colorbox({
+			rel:'group', 
+			transition:'elastic', 
+			preloading:'true', 
+			closebutton:'true', 
+			maxHeight: '80%', 
+			maxWidth: '80%',
+			fixed: 'true',
+        		scrolling:'false',
+        		onOpen: function(){
+             			$("#cboxPhoto").css("opacity", 0);
+                	},
+        		onComplete: function(){
+             			$("#cboxPhoto").animate({"opacity": 1});
+        		}
+	});
+
+	}
+}
+
+$(jsFrontend.projectsDetail.init);
